d53b42eb09ae3839340e3ed4239418464988ae7a
Tran Thu Hue <94huetran@gmail.com>
2018-07-09 11:20:04 +0700

    add BinarySearch

---
+++ src/main/java/BinarySearch/BinarySearch.java
@@ -0,0 +1,26 @@
+package BinarySearch;
+
+public class BinarySearch {
+    static int[] list = {2, 3, 4, 5, 11, 34, 45, 50, 59, 70};
+
+    static int binarySearch(int[] list, int key) {
+        int low = 0;
+        int high= list.length-1;
+        while (high >= low) {
+            int mid = (low + high) / 2;
+
+            if (key < list[mid]) {
+                high = mid -1;
+            } else if (key == list[mid]) {
+                return mid;
+            }else low = mid+1;
+        } return -1;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(binarySearch(list,11));
+        System.out.println(binarySearch(list, 45));
+        System.out.println(binarySearch(list,90));
+    }
+}
